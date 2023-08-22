20d633e39445228ebc5e77fde423305e65f3dcc1
simon y <71997308+kjai139@users.noreply.github.com>
2023-08-22 01:04:24 -0400

    add mongo

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+const app = express()
+const port = process.env.PORT || 4000
+const mongodb = process.env.MONGO_LOGIN
+
+const main = async () => {
+    try {
+        mongoose.connect(mongodb)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
